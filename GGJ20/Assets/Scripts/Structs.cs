60b18a23c120f852c37cb0070d005adea7adc851
Berlauch <luca.oertig@gmail.com>
2020-02-01 00:23:48 +0100

    Json

---
+++ GGJ20/Assets/Scripts/Structs.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Weatherdata
+{
+	public Lonlat coord;
+	public Weather weather;
+	public Data main;
+	public Wind wind;
+	public Cloud clouds;
+	public Water rain;
+	public Snow snow;
+	public Sun sys;
+
+	public struct Lonlat
+	{
+		public double lon;
+		public double lat;
+	}
+
+	public struct Weather
+	{
+		public int id;
+		public string main;
+		public string description;
+		public string icon;
+	}
+	public Weather[] weathers;
+
+	public string _base;
+
+	public struct Data
+	{
+		public double temp;
+		public double feels_like;
+		public double pressure;
+		public double humidity;
+		public double temp_min;
+		public double temp_max;
+		public double sea_level;
+		public double grnd_level;
+	}
+
+	public struct Wind
+	{
+		public double speed;
+		public double deg;
+	}
+
+	public struct Cloud
+	{
+		public double all;
+	}
+
+	public struct Water
+	{
+		public double _1h;
+		public double _3h;
+	}
+
+	public struct Snow
+	{
+		public double _1h;
+		public double _3h;
+	}
+
+	public int dt;
+
+	public struct Sun
+	{
+		public string type;
+		public int id;
+		public double message;
+		public string country;
+		public int sunrise;
+		public int sunset;
+	}
+
+	public int timezone;
+
+	public int id;
+
+	public string name;
+
+	public int cod;
+}
---
+++ GGJ20/Assets/Scripts/WWWTrial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+public class WWWTrial : MonoBehaviour
+{
+	// Start is called before the first frame update
+	[System.Obsolete]
+	IEnumerator Start()
+	{
+		string url = "http://api.openweathermap.org/data/2.5/weather?lat=0&lon=12&appid=634a27863c00b54741b8171aff512e22";
+		UnityWebRequest www = UnityWebRequest.Get(url);
+		yield return www.SendWebRequest();
+		if (www.error == null)
+
+		{
+			var data = (www.downloadHandler.text);
+
+			double cloud = data.clouds.all;
+
+			string latitude = data["coord.lat"].Value<string>();
+			double rain = data.rain._3h;
+			string sun = data.weather.description;
+			Debug.Log(data.clouds.all);
+		}
+    }
+}
