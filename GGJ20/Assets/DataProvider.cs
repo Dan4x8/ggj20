08a45d4079d8318c06ae200725499aab1394a980
Berlauch <luca.oertig@gmail.com>
2020-02-02 15:01:03 +0100

    shop

--- GGJ20/Assets/DataProvider.cs
+++ GGJ20/Assets/DataProvider.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 public class DataProvider : MonoBehaviour
 {
 	public CauseDeath cause;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Start()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
 }
